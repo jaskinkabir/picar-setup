817ee18389efd9abf1d6b8c93699d68efac6843b
Jaskin Kabir <jkabir@uncc.edu>
2025-05-29 20:22:16 -0400

    Update setup.sh with rm

--- setup.sh
+++ setup.sh
@@ -1,31 +1,26 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y python3 python3-pip python3-venv git python3-setuptools python3-smbus
-
-
-echo "---------INSTALLING VSCODE----------"
-cd ~/
-mkdir downloads
-cd downloads
-# download vscode https://go.microsoft.com/fwlink/?LinkID=760868
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
-sudo apt install -y ./vscode.deb
-
 
 echo "---------INSTALLING ROBOT HAT----------"
 cd ~/
 git clone -b v2.0 https://github.com/sunfounder/robot-hat.git
 cd robot-hat
 sudo python3 setup.py install
+cd ~/
+sudo rm -rf robot-hat
 
 echo "---------INSTALLING VILIB----------"
 cd ~/
 git clone -b picamera2 https://github.com/sunfounder/vilib.git
 cd vilib
-sudo python3 setup.py install
+sudo python3 install.py
+cd ~/
+sudo rm -rf vilib
 
 echo "---------INSTALLING PiCar-X----------"
 cd ~/
 git clone -b v2.0 https://github.com/sunfounder/picar-x.git --depth 1
 cd picar-x
 sudo python3 setup.py install
+cd ~/
+sudo rm -rf picar-x
